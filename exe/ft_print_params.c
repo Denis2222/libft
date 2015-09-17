15e031a671674e2476bad08941e3157cb7216b82
Denis <denismoureu@gmail.com>
2015-09-17 23:29:01 +0200

    fix syntax

--- exe/ft_print_params.c
+++ exe/ft_print_params.c
@@ -4,11 +4,11 @@ int	main(int argc, char **argv)
 {
 	int	arg;
 	arg = 1;
-while (arg < argc)
-{
-	ft_putstr(argv[arg]);
-	ft_putchar('\n');
-	arg++;
-}
+	while (arg < argc)
+	{
+		ft_putstr(argv[arg]);
+		ft_putchar('\n');
+		arg++;
+	}
 	return (0);
 }
