65e2e7ef60c9e9ec3c9b7b1df6881e53fca064c1
Denis <denismoureu@gmail.com>
2015-09-19 16:59:35 +0200

    ft_split

--- src/ft_split.c
+++ src/ft_split.c
@@ -1,43 +1,90 @@
 #include <libft.h>
 
-void	ft_split(char *str, char *charset)
+int		ft_split_count_words(char *str, char *charset)
 {
-
 	int	c;
-	int	a;
-	int nbcharset;
-	int	new;
-	int words;
+	int nb;
+	int word;
+	int lword;
 
-	a = 0;
 	c = 0;
-	new = 0;
-	nbcharset = 0;
+	nb = 0;
+	word = 0;
+	lword = 0;
 	while (str[c] != '\0')
 	{
-		if (str[c] == charset[a])
+		if(ft_strncmp(&str[c], charset, ft_strlen(charset)) == 0)
 		{
-			a++;
-			if (charset[a] == '\0')
-			{
-				new = 0;
-				nbcharset++;
-				a = 0;
-			}
+			nb++;
+			c += ft_strlen(charset);
+			lword = 0;
 		}
-		else if (str[c] == charset[0])
-			a = 1;
 		else
 		{
-			a = 0;
-			if (!new)
-			{
-				new++;
-				words++;
-			}
+			if(!lword)
+				word++;
+			lword++;
+			c++;
 		}
-		c++;
 	}
+	return (word);	
+}
 
-	printf("nbcharset:%d word:%d", nbcharset, words);
+char	*ft_split_get_word(char *str, char *charset, int nword)
+{
+	int	c;
+	int nb;
+	int	word;
+	int lword;
+	char *strword;
+
+	c = 0;
+	nb = 0;
+	word = 0;
+	lword = 0;
+	while (str[c] != '\0')
+	{
+		if(ft_strncmp(&str[c], charset, ft_strlen(charset)) == 0)
+		{
+			//printf(" word: [%d] (%d)  ", word, (c-lword));
+			if (word == nword)
+			{
+				strword = malloc(sizeof(char *) * (lword + 1));
+				return (ft_strncpy(strword, &str[c-(lword)],lword));
+			}
+			nb++;
+			c += ft_strlen(charset);
+			lword = 0;
+		}
+		else
+		{
+			if(!lword)
+				word++;
+			lword++;
+			c++;
+		}
+	}
+	return (str);
+}
+
+char **ft_split(char *str, char *charset)
+{
+	char **tab;
+	int	nbword;
+	int i;
+
+	printf("-->%s<--", ft_strdup(ft_split_get_word(str, charset, 1)));
+
+	nbword = ft_split_count_words(str, charset);
+	tab = malloc(sizeof(char *) * (nbword + 1));
+	i = 0;
+	while (i < nbword)
+	{
+		tab[i] = ft_strdup(ft_split_get_word(str, charset, i));
+		ft_putstr(tab[i]);
+		ft_putstr("\n");
+		i++;
+	}
+	printf("nb mots: %d", nbword);
+	return (tab);
 }
