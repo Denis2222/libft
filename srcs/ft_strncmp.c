33c31c7d547543eb6a119fdcf39edcfb3fd66330
Denis MOUREU-LARRANG <dmoureu-@e1r5p19.42.fr>
2015-11-23 17:22:25 +0100

    strncmp correction

--- srcs/ft_strncmp.c
+++ srcs/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: dmoureu- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/23 11:31:10 by dmoureu-          #+#    #+#             */
-/*   Updated: 2015/11/23 11:49:11 by dmoureu-         ###   ########.fr       */
+/*   Updated: 2015/11/23 17:21:22 by dmoureu-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,5 @@ int	ft_strncmp(char *s1, const char *s2, size_t n)
 	x = 0;
 	while (s1[x] == s2[x] && s1[x] != '\0' && s2[x] != '\0' && x < (n - 1))
 		x++;
-	return (s1[x] - s2[x]);
+	return ((unsigned char)s1[x] - (unsigned char)s2[x]);
 }
