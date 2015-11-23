86ec472537c37e4ce52eafd7bf61a8fb6b945bd5
Denis MOUREU-LARRANG <dmoureu-@e1r5p19.42.fr>
2015-11-23 17:05:30 +0100

    correction ft_strnstr

--- srcs/ft_strnstr.c
+++ srcs/ft_strnstr.c
@@ -6,39 +6,43 @@
 /*   By: dmoureu- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/23 11:31:24 by dmoureu-          #+#    #+#             */
-/*   Updated: 2015/11/23 13:03:57 by dmoureu-         ###   ########.fr       */
+/*   Updated: 2015/11/23 17:04:23 by dmoureu-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 
+void	ft_strnstr_init(size_t *pos, int *x)
+{
+	pos = 0;
+	x = 0;
+}
+
 char	*ft_strnstr(char *str, const char *to_find, size_t n)
 {
 	size_t	pos;
 	int		x;
-	size_t	length;
 
 	if (ft_strlen((char *)to_find) == 0)
 		return (str);
-	length = 0;
 	pos = 0;
 	x = 0;
 	while (str[pos] != '\0')
 	{
-		if (str[pos] == to_find[x++])
+		if (str[pos] == to_find[x])
 		{
-			length++;
+			x++;
 			if (to_find[x] == '\0')
 				return (&str[pos - x + 1]);
 		}
 		else
 		{
+			pos = pos - x;
 			x = 0;
-			length = 0;
 		}
+		pos++;
 		if (pos >= n)
 			return (NULL);
-		pos++;
 	}
-	return (0);
+	return (NULL);
 }
