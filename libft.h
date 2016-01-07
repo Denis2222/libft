cbd3ac49860ff326a1c97d2bc0cc6f7fe327264b
Denis2222 <denismoureu@gmail.com>
2016-01-07 10:19:56 +0100

    grrr

--- libft.h
+++ libft.h
@@ -71,14 +71,12 @@ char				*ft_strsub(char const *s, unsigned int start, size_t len);
 char				*ft_strjoin(char const *s1, char const *s2);
 char				*ft_strtrim(char const *s);
 char				**ft_strsplit(char const *s, char c);
-int					ft_tablen(char **tab);
 char				*ft_itoa(int n);
 void				ft_putendl(char const *s);
 void				ft_putchar_fd(char c, int fd);
 void				ft_putstr_fd(char const *s, int fd);
 void				ft_putendl_fd(char const *s, int fd);
 void				ft_putnbr_fd(int n, int fd);
-int					ft_sqrt(int nb);
 
 typedef struct		s_list
 {
